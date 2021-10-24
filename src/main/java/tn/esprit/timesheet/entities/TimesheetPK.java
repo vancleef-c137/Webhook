5ea93fe1f32dcb2a38a108559ea7e4db8549e168
Akai <takwa.aissa@esprit.tn>
2021-10-24 13:48:22 +0100

    adding some test feature

--- src/main/java/tn/esprit/timesheet/entities/TimesheetPK.java
+++ src/main/java/tn/esprit/timesheet/entities/TimesheetPK.java
@@ -17,10 +17,8 @@ public class TimesheetPK implements Serializable {
 	private int idEmploye;
 	
 	//Choisir le TemporalType selon le besoin metier
-	@Temporal(TemporalType.DATE)
 	private LocalDate dateDebut;
 	
-	@Temporal(TemporalType.DATE)
 	private LocalDate dateFin;
 	
 
